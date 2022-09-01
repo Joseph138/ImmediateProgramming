9a666cfd77d068dec7e609c90282b218ad131e65
hpark4 <hpark4@gmail.com>
2022-08-31 20:03:40 -0400

    revised.

--- Points.java
+++ Points.java
@@ -5,14 +5,33 @@ public class Points {
                 Scanner input = new Scanner(System.in);
                 System.out.println("How many pairs of x and y do you want to enter");
                 int amount = input.nextInt();
-                int x = input.nextInt();
-                int y= input.nextInt();
-                int [][]pairs = [x][y];
-                for(int i = 0; i < pairs.length;i++){
-                    for(int j = 0; j < pairs[i].length; j++){
 
-                    }
+                double[][] points = new double[amount][2]; //amount is the number
+        // of pairs and 2 is x and y
+
+        for(int i = 0; i < amount; i++ )
+        {
+            points[i][0] = input.nextDouble();
+            points[i][1] = input.nextDouble();
+
+        /**
+         * index of number 1 of the first array is points[0][0]
+         * index of number 2 of the first array is points[0][1]
+         * index of number  5 of the second array is points[1][0]
+         * index of number 6 of the second array is points[1][1]
+         *
+         * {{1,2},
+         * {5,6},
+         * {4,8}}
+         */
+//        int x = input.nextInt();
+//                int y= input.nextInt();
+//                int [][]pairs = [x][y];
+//                for(int i = 0; i < pairs.length;i++){
+//                    for(int j = 0; j < pairs[i].length; j++){
+//
+//                    }
                 }
-       System.out.println("The output of your pairs are " + Arrays.deepToString(pairs));
+       System.out.println("The output of your pairs are " + Arrays.deepToString(points));
    }
 }
